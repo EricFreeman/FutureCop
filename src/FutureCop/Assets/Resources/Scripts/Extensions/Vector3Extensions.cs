6c788c54e361f7812a590cdfe5a67e57ea6cc186
Eric Freeman <clownhunter@gmail.com>
2016-10-08 01:45:51 -0600

    Check for walls before moving to avoid that stupid sticking to wall issue with unity2d.

---
+++ src/FutureCop/Assets/Resources/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Resources.Scripts.Extensions
+{
+    public static class Vector3Extensions
+    {
+        public static Vector2 ToVector2(this Vector3 vector)
+        {
+            return new Vector2(vector.x, vector.y);
+        }
+    }
+}
--- src/FutureCop/Assets/Resources/Scripts/Player/PlayerMovement.cs
+++ src/FutureCop/Assets/Resources/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using Assets.Resources.Scripts.Extensions;
+using UnityEngine;
 
 namespace Assets.Resources.Scripts.Player
 {
@@ -30,20 +32,9 @@ namespace Assets.Resources.Scripts.Player
 
             CalculateMovement();
             CalculateJump();
+            CheckForWallCollision();
 
             _rigidbody.velocity = new Vector2(_movement + _knockback.x, _rigidbody.velocity.y + _jump + _knockback.y);
-        }
-
-        private void CalculateJump()
-        {
-            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-            {
-                _jump = Jump;
-            }
-            else
-            {
-                _jump = 0;
-            }
         }
 
         private void CalculateMovement()
@@ -59,7 +50,39 @@ namespace Assets.Resources.Scripts.Player
             }
             else
             {
-                _movement *= .7f;
+                _movement *= .4f;
+            }
+        }
+
+        private void CalculateJump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            {
+                _jump = Jump;
+            }
+            else
+            {
+                _jump = 0;
+            }
+        }
+
+        private void CheckForWallCollision()
+        {
+            var bounds = transform.GetComponent<SpriteRenderer>().bounds;
+            var start = bounds.center;
+            var length = _rigidbody.velocity.x * Time.deltaTime;
+            var height = bounds.extents.y - .05f;
+
+            RaycastHit2D[] hits =
+            {
+                Physics2D.Raycast(start + new Vector3(bounds.extents.x * (_movement > 0 ? 1 : -1), height), _movement > 0 ? Vector3.right : Vector3.left, length, 1 << LayerMaskForGrounded.value),
+                Physics2D.Raycast(start + new Vector3(bounds.extents.x * (_movement > 0 ? 1 : -1), 0), _movement > 0 ? Vector3.right : Vector3.left, length, 1 << LayerMaskForGrounded.value),
+                Physics2D.Raycast(start + new Vector3(bounds.extents.x * (_movement > 0 ? 1 : -1), -height), _movement > 0 ? Vector3.right : Vector3.left, length, 1 << LayerMaskForGrounded.value)
+            };
+
+            if (hits.Any(x => x))
+            {
+                _movement = 0;
             }
         }
 
@@ -69,10 +92,14 @@ namespace Assets.Resources.Scripts.Player
             var start = bounds.center - new Vector3(0, bounds.extents.y);
             var length = bounds.extents.y / 2;
 
-            Debug.DrawRay(start, Vector3.down * length);
-            var hit = Physics2D.Raycast(start, Vector3.down, length, 1 << LayerMaskForGrounded.value);
+            RaycastHit2D[] hits =
+            {
+                Physics2D.Raycast(start, Vector3.down, length, 1 << LayerMaskForGrounded.value),
+                Physics2D.Raycast(start - new Vector3(bounds.extents.x, 0), Vector3.down, length, 1 << LayerMaskForGrounded.value),
+                Physics2D.Raycast(start + new Vector3(bounds.extents.x, 0), Vector3.down, length, 1 << LayerMaskForGrounded.value)
+            };
             
-            return hit;
+            return hits.Any(x => x);
         }
     }
 }
